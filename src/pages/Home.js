04caac76ea9f33888077eca7ceef84ddb26b2f46
MichaelJRossDev <michaeljrossdev@gmail.com>
2023-05-10 14:40:09 +0100

    Tinker with homepage some more

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,8 +9,15 @@ function Home() {
             <HomeSwiper />
             <div className="ContentContainer1">
                 <div className="ContainerSplit">
-                    <h1>Title</h1>
-                    <p>Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content </p>
+                    <h1>Shaman DIY craft beer blackbird spyplane keytar live-edge listicle four loko.</h1>
+                    <p>Yuccie pinterest yr meditation, kale chips live-edge wayfarers art party. Gatekeep next level knausgaard, put a bird on it vegan vice trust fund truffaut. Helvetica keffiyeh health goth, echo park adaptogen vibecession humblebrag knausgaard hammock iceland enamel pin</p>
+                    <ul>
+                        <strong>
+                            <li>Cold-pressed air plant waistcoat gastropub sartorial</li>
+                            <li>Hella tumblr shabby chic pug church-key williamsburg jianbing</li>
+                            <li>VHS gentrify cardigan truffaut drinking vinegar normcore pork belly lyft</li>
+                        </strong>
+                    </ul>
                     <Link to='/about-us' className="ButtonContainer">
                         <button>Learn more</button>
                     </Link>
