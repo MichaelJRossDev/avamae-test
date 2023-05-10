3ab1cdf44be58a8d1c0f8ad3730bcb855f21ff72
MichaelJRossDev <michaeljrossdev@gmail.com>
2023-05-10 13:49:25 +0100

    Start dynamic layout on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,25 @@
 import HomeSwiper from "../components/HomeSwiper";
+import './HomeStyles.css';
+import image from '../Resources/shutterstock_696636415.jpg'
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
+        <>
             <HomeSwiper />
+            <div className="ContentContainer1">
+                <div className="ContainerSplit">
+                    <h1>Title</h1>
+                    <p>Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content </p>
+                    <Link to='/about-us' className="ButtonContainer">
+                        <button>Learn more</button>
+                    </Link>
+                </div>
+                <div className="ContainerSplit">
+                    <img src={image}/>
+                </div>
+            </div>
+        </>
     )
 }
 
