555ac546a15c98e3da9d392624e4990e1b3774e2
MichaelJRossDev <michaeljrossdev@gmail.com>
2023-05-10 23:26:39 +0100

    Begin contact us page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,7 +1,17 @@
+import contactUsImage from "../Resources/Img_Contact.png"
+
 function ContactUs() {
     return (
         <>
-            <h1>ContactUs</h1>
+            <div className="ContactUsContainer">
+                <div>
+                    <h1>Contact us</h1>
+                    <p>Air plant squid cronut, fingerstache biodiesel pabst mukbang neutral milk hotel meh bruh actually tofu. Lumbersexual four dollar toast sus post-ironic</p>
+                </div>
+                <div>
+                    <img src={contactUsImage}/>
+                </div>
+            </div>
         </>
     )
 }
