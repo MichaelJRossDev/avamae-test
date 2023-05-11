d7a44376547017cbca0f740cfbe12cfcba71bd14
MichaelJRossDev <michaeljrossdev@gmail.com>
2023-05-11 10:38:16 +0100

    Made some progress with contact us page graphic

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,17 +1,14 @@
 import contactUsImage from "../Resources/Img_Contact.png"
+import "./ContactUsStyles.css"
 
 function ContactUs() {
     return (
         <>
             <div className="ContactUsContainer">
-                <div>
                     <h1>Contact us</h1>
                     <p>Air plant squid cronut, fingerstache biodiesel pabst mukbang neutral milk hotel meh bruh actually tofu. Lumbersexual four dollar toast sus post-ironic</p>
-                </div>
-                <div>
-                    <img src={contactUsImage}/>
-                </div>
             </div>
+            <img className="ContactUsImage" src={contactUsImage}/>
         </>
     )
 }
